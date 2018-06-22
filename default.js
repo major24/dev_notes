869242bd07ac197166de44fdad601cafb5bba83a
mnalliah <major.nalliah@gmail.com>
2018-06-21 22:02:14 -0400

    added default js filel

--- default.js
+++ default.js
@@ -5,3 +5,12 @@ function getuser(id){
 	var x = this.service.get_user(id);
 	return x;
 }
+
+function getproduct(id){
+	var p = this.prod_service.get_product(id);
+	return p;
+}
+
+function getdatetime(){
+	return datetime();
+}
